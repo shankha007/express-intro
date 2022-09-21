a09831f85a50033d18f8f95d8f2010b114b6bb7d
Shankha <ribhumaster@gmail.com>
2022-09-21 18:26:25 +0530

    assignment done

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.use("/add-product", (req, res, next) => {
-  console.log("In first middleware");
-  res.send("<h1>In 'Add Product' Page!</h1>");
+// app.use("/products", (req, res, next) => {
+//   console.log("In the products page");
+//   next();
+// });
+
+// app.use("/", (req, res, next) => {
+//   console.log("In home page");
+//   res.send("<h1>Welcome to Home Page</h1>");
+// });
+
+app.use("/users", (req, res, next) => {
+  res.send("<h1>In the Users page</h1>");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In another middleware");
-  res.send("<h1>Hello from Express.js!</h1>");
+  res.send("<h1>In the Home Page</h1>");
 });
 
 app.listen(3000);
