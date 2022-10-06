2ed6875e35eb12e12248041b09d0d491fb51a4f4
Shankha <ribhumaster@gmail.com>
2022-10-07 01:13:41 +0530

    assignment 2 completed

--- routes/home.js
+++ routes/home.js
@@ -7,7 +7,7 @@ const rootDir = require("../utils/path");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  res.sendFile(path.join(rootDir, "views", "home.html"));
 });
 
 module.exports = router;
