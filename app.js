bda717c0344a0767706739822239316e845ab9f9
Shankha <ribhumaster@gmail.com>
2022-10-07 19:29:58 +0530

    used pug as a templating engine

--- app.js
+++ app.js
@@ -3,19 +3,23 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
 
+app.set("view engine", "pug");
+// if you save html files in some other folder change the second views with that folder name
+// app.set("views", "views");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
