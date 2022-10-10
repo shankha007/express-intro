772f31c8c118af40feab038665ab74a4d1884c07
Shankha <ribhumaster@gmail.com>
2022-10-10 21:56:09 +0530

    Assignment done

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-const path = require("path");
-
-const express = require("express");
-
-const usersRoutes = require("./routes/users");
-const homeRoutes = require("./routes/home");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+const users = [];
 
-app.use(usersRoutes);
-app.use(homeRoutes);
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res, next) => {
+  res.render('index', { pageTitle: 'Add User' });
+});
+
+app.get('/users', (req, res, next) => {
+  res.render('users', {
+    pageTitle: 'User',
+    users: users,
+    hasUsers: users.length > 0
+  });
+});
+
+app.post('/add-user', (req, res, next) => {
+  users.push({ name: req.body.username });
+  res.redirect('/users');
+});
 
 app.listen(3000);
