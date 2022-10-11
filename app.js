55fe07c2da7d517b953a244dae28d225b83d287b
Shankha <ribhumaster@gmail.com>
2022-10-11 21:56:06 +0530

    MVC architecture built

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/error");
 
 const app = express();
 
@@ -15,14 +16,9 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", {
-    pageTitle: "Page Not Found",
-    path: '404'
-  });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
