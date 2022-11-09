4cd061c58dc122260a3e4cad90c63dc3ff648c80
Shankha <ribhumaster@gmail.com>
2022-11-09 21:02:49 +0530

    dynamic routes and advanced models

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,17 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    res.render("shop/product-detail", {
+      product,
+      pageTitle: product.title,
+      path: "/products",
+    });
+  });
+};
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("shop/index", {
@@ -25,6 +36,10 @@ exports.getCart = (req, res, next) => {
     path: "/cart",
     pageTitle: "Your Cart",
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
 };
 
 exports.getOrders = (req, res, next) => {
